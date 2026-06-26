a71a11799ac78d54fa1f2c1ea41a5d1b33c3870a
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-06-26 06:24:00 +0530

    Time: 1 ms (99.45%) | Memory: 47.2 MB (31.65%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length] ;
-        for(int i =0;i<nums.length;i++){
-              ans[i]=nums[nums[i]];
+        int[] arr = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i] = nums[nums[i]];
         }
-        return ans;
-    }
+        return arr;
+    }   
 }
