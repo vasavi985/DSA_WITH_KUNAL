df0e474dc69d1139ea9ff6eba03ca3853000af89
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-06-30 15:57:20 +0530

    Time: 2 ms (63.21%) | Memory: 43 MB (34.40%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] alphabet = new boolean[26];
+        for(int i =0;i<sentence.length();i++){
+            char ch = sentence.charAt(i);
+            alphabet[ch - 'a'] = true;
+
+        }
+        for(int i=0;i<26;i++){
+            if(!alphabet[i]){
+                return false;
+            }
+
+        }
+    
+        return true;
+    }
+}
