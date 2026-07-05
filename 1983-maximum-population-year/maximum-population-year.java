25efcd3f7f605a82ab42352c87413fdfd3f3910e
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-07-05 07:47:16 +0530

    Time: 3 ms (25.74%) | Memory: 43.6 MB (34.02%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.java
+++ 1983-maximum-population-year/maximum-population-year.java
@@ -1,21 +1,22 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        int max=0;
-        int ans=1950;
-        for(int year=1950;year<=2050;year++){
+         int max=0;
+         int ans=1950;
+         for(int year=1950;year<=2050;year++){
             int count=0;
             for(int i=0;i<logs.length;i++){
                 int birth=logs[i][0];
-                int death = logs[i][1];
+                int death=logs[i][1];
                 if(year>=birth && year<death){
-                    count++;
+                    count+=1;
+                }
+                if(count>max){
+                    max=count;
+                    ans=year;
                 }
             }
-            if(count>max){
-                max=count;
-                ans=year;
-            }
-        }
-       return ans;
+
+         }
+          return ans;
     }
 }
