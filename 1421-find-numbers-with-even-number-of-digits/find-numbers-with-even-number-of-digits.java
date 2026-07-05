cc5a26c9a1baed0bdbacb33806fa19c39d7e827f
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-07-05 07:22:44 +0530

    Time: 1 ms (99.53%) | Memory: 44.9 MB (21.82%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,19 +1,17 @@
 class Solution {
     public int findNumbers(int[] nums) {
-        int res=0;
+         int result=0;
         for(int i=0;i<nums.length;i++){
             int count=0;
-            int num=nums[i];
-            while(num>0){
-                num=num/10;
+            int n = nums[i];
+            while(n>0){
+                n=n/10;
                 count+=1;
             }
             if(count%2==0){
-
-            res+=1;
-
+                result+=1;
             }
         }
-        return res;
+        return result;
     }
 }
