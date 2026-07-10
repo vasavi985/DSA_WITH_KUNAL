e79b5a3b39243cf78176e28812a336c977165298
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-07-10 07:45:06 +0530

    Time: 4 ms (96.89%) | Memory: 49.9 MB (13.00%) - LeetSync

--- 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
@@ -1,24 +1,20 @@
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-         int column=0;
-         if(ruleKey.equals("type")){
+        int column=0;
+        if(ruleKey.equals("type")){
             column=0;
-         }
+        }
         if(ruleKey.equals("color")){
             column=1;
         }
-
         if(ruleKey.equals("name")){
             column=2;
         }
         int count=0;
         for(int i=0;i<items.size();i++){
-             
-                if(items.get(i).get(column).equals(ruleValue)){
-                    count+=1;
-                }
-
-            
+            if(items.get(i).get(column).equals(ruleValue)){
+                count+=1;
+            }
         }
         return count;
     }
