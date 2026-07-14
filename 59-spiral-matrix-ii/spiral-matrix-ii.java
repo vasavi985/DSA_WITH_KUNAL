235268ed2a8909866ec9dbbb7fb5061e1a312219
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-07-14 06:29:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.59%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] matrix = new int[n][n];
+        int[][] matrix = new  int[n][n];
         int top=0;
         int bottom=matrix.length-1;
-        int left=0;
-        int right=matrix[0].length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
         int num=1;
         while(left<=right && top<=bottom){
-        
             for(int col=left;col<=right;col++){
                 matrix[top][col]=num++;
-            
             }
             top++;
             for(int row=top;row<=bottom;row++){
@@ -18,20 +16,18 @@ class Solution {
             }
             right--;
             if(top<=bottom){
-                for(int col = right;col>=left;col--){
-                    matrix[bottom][col]=num++;
-                }
-                bottom--;
+            for(int col=right;col>=left;col--){
+                matrix[bottom][col]=num++;
+            }
+            bottom--;
             }
             if(left<=right){
-                for(int row=bottom;row>=top;row--){
-                    matrix[row][left]=num++;
-                }
-                left++;
+            for(int row = bottom;row>=top;row--){
+                matrix[row][left]=num++;
             }
-        
+            left++;
+            }
         }
-        
-return matrix;
+         return matrix;
     }
 }
