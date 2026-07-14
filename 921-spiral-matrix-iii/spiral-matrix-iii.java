bb9b76e9fa2c9175e79656a386a23b86d2f7d2f8
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-07-14 05:57:16 +0530

    Time: 3 ms (97.68%) | Memory: 47 MB (78.17%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -4,48 +4,49 @@ class Solution {
         ans[0][0]=rStart;
         ans[0][1]=cStart;
         int index=1;
-        int r = rStart;
-        int c = cStart;
         int steps=1;
-        while (index < rows * cols) {
-        for (int i = 0; i < steps; i++) {
-             c++;
-             if(r>=0 && r<rows && c>=0 && c<cols){
-                ans[index][0] = r;
-                ans[index][1]=c;
-                index++;
-             }
-        }
-        for(int i=0;i<steps;i++){
-            r++;
-            if(r>=0 && r<rows && c>=0 && c<cols ){
-                ans[index][0]=r;
-                ans[index][1]=c;
-                index++;
+        int r =rStart;
+        int c=cStart;
+        while(index<rows*cols){
+            for(int i=0;i<steps;i++){
+                c++;
+                if(r>=0 && r<rows && c>=0 && c<cols){
+                    ans[index][0]=r;
+                    ans[index][1]=c;
+                    index++;
+                }
             }
+            for(int i=0;i<steps;i++){
+                r++;
+                if(r>=0 && r<rows && c<cols && c>=0){
+                    ans[index][0]=r;
+                    ans[index][1]=c;
+                    index++;
+                }
+            }
+            steps++;
+            for(int i=0;i<steps;i++){
+                c--;
+                if(r>=0 && r<rows && c<cols && c>=0){
+                    ans[index][0]=r;
+                    ans[index][1]=c;
+                    index++;
+                }
+            }
+             for(int i=0;i<steps;i++){
+                r--;
+                if(r>=0 && r<rows && c<cols && c>=0){
+                    ans[index][0]=r;
+                    ans[index][1]=c;
+                    index++;
+                }
+            }
+            steps++;
+        
 
         }
-         steps++;
-         for(int i=0;i<steps;i++){
-            c--;
-             if(r>=0 && r<rows && c>=0 && c<cols ){
-                ans[index][0]=r;
-                ans[index][1]=c;
-                index++;
-            }
-         }
-         for(int i=0;i<steps;i++){
-            r--;
-             if(r>=0 && r<rows && c>=0 && c<cols ){
-                ans[index][0]=r;
-                ans[index][1]=c;
-                index++;
-            }
-         }
-steps++;
-        }
+         
 return ans;
-
         
     }
 }
