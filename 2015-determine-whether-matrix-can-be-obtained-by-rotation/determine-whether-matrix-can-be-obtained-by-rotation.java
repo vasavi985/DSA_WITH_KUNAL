a650878374c445a7cb4dea585ccfad6a6159af9f
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-07-29 06:37:57 +0530

    Time: 1 ms (38.59%) | Memory: 43.9 MB (9.13%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -2,41 +2,38 @@ import java.util.Arrays;
 
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        if(Arrays.deepEquals(mat,target)){
+         if(Arrays.deepEquals(mat,target)){
             return true;
-        }
-int[][] ans = mat;
-// for 90 180 270 degrees
-for(int k=0;k<3;k++){
-    //transpose of a matrix
-    int row=ans.length;
-    int col=ans[0].length;
-    int[][] res = new int[col][row];
-    for(int i=0;i<ans.length;i++){
-        for(int j=0;j<ans[0].length;j++){
-            res[j][i]=ans[i][j];
-        }
-    }
-    //reverse of each row
-    for(int i=0;i<res.length;i++){
-        int left=0;
-        int right=res[i].length-1;
-        while(left<right){
-            int temp=res[left][i];
-            res[left][i]=res[right][i];
-            res[right][i]=temp;
-            left++;
-            right--;
-        }
-
-    }
-    if(Arrays.deepEquals(res,target)){
-        return true;
-    }
-ans=res;
+         }
+         int[][] ans = mat;
+         for(int k=0;k<3;k++){
+            int row = ans.length;
+            int col = ans[0].length;
+            int[][] res = new int[row][col];
+            // transpose
+            for(int i=0;i<row;i++){
+                for(int j=0;j<col;j++){
+                    res[j][i]=ans[i][j];
+                }
+            }
+//reverse
+for(int i=0;i<ans.length;i++){
+int left=0;
+int right=ans[i].length-1;
+while(left<right){
+    int temp = res[i][left];
+    res[i][left]=res[i][right];
+    res[i][right]=temp;
+    left++;
+    right--;
 
 }
-return false;
-        
+}
+if(Arrays.deepEquals(res,target)){
+    return true;
+}
+ans=res;
+         }
+         return false;
     }
 }
