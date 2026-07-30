0752a1ef594fd862f6aa84b7db0a295bc92e94a1
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-07-30 06:39:33 +0530

    Time: 1 ms (3.38%) | Memory: 43.6 MB (23.64%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,39 +1,29 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        ArrayList<Integer> ans =new ArrayList<>();
         int i =digits.length-1;
-        int k=1;
         int carry=0;
-        while(i>=0||k>0||carry>0){
-            int sum=carry;
+        ArrayList<Integer> ans = new ArrayList<>();
+        int k=1;
+        while(i>=0||carry>0||k>0){
+            int sum = carry;
             if(i>=0){
                 sum+=digits[i];
             }
             if(k>0){
-                int digit=k%10;
+                int digit = k%10;
                 sum+=digit;
             }
-ans.add(sum%10);
-carry=sum/10;
-k=k/10;
-i--;
+            ans.add(sum%10);
+            carry=sum/10;
+            k=k/10;
+            i--;
         }
-        
-            int left=0;
-            int right = ans.size()-1;
-            while(left<right){
-            int temp = ans.get(left);
-            ans.set(left, ans.get(right));
-            ans.set(right, temp);
-                left++;
-                right--;
-
-            }
-        
-        int[] answer = new int[ans.size()];
+        Collections.reverse(ans);
+    
+        int[] res = new int[ans.size()];
         for(int j=0;j<ans.size();j++){
-            answer[j]=ans.get(j);
+            res[j]=ans.get(j);
         }
-        return answer;
+        return res;
     }
 }
