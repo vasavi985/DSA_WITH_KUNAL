30266762f2c5c40dfdab44ea71bfd02d33306ba3
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-07-31 08:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (80.59%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,33 +1,34 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] matrix = new  int[n][n];
-        int top=0;
-        int bottom=matrix.length-1;
-        int left = 0;
-        int right = matrix[0].length-1;
+        int[][] matrix = new int[n][n];
         int num=1;
+        int left=0;
+        int right = matrix[0].length-1;
+        int top = 0;
+        int bottom=matrix.length-1;
         while(left<=right && top<=bottom){
             for(int col=left;col<=right;col++){
                 matrix[top][col]=num++;
             }
             top++;
-            for(int row=top;row<=bottom;row++){
-                matrix[row][right]=num++;
+            for(int row = top;row<=bottom;row++){
+                matrix[row][right] = num++;
             }
             right--;
             if(top<=bottom){
-            for(int col=right;col>=left;col--){
-                matrix[bottom][col]=num++;
-            }
-            bottom--;
+                for(int col = right;col>=left;col--){
+                    matrix[bottom][col]=num++;
+                }
+                bottom--;
             }
             if(left<=right){
-            for(int row = bottom;row>=top;row--){
-                matrix[row][left]=num++;
-            }
-            left++;
+                for(int row =bottom;row>=top;row--){
+                    matrix[row][left]=num++;
+                }
+                left++;
             }
         }
-         return matrix;
+
+        return matrix;
     }
 }
