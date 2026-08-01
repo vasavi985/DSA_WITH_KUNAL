f728fe04b5bb1e900f61f51e027e1392246a47cc
Rama Vasavi patchikolla <vasavi0908@gmail.com>
2026-08-01 15:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (74.77%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int max =0;
-       for(int i=0;i<accounts.length;i++){
-        int sum=0;
-        for(int j=0;j<accounts[i].length;j++){
-            sum+=accounts[i][j];
+        int sum =0;
+        for(int i=0;i<accounts.length;i++){
+            int sum_col =0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum_col+=accounts[i][j];
+
+            }
+            if(sum_col>sum){
+                sum=sum_col;
+            }
         }
-        if(sum>max){
-            max=sum;
-        }
-       } 
-         
-        return max;
+        return sum;
     }
 }
